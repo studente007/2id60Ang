e415aa8e6e78cf7a0bcdccc98ee78dfc22bd1f3d
Sheng <stshing@hotmail.com>
2017-01-08 18:37:33 +0100

    new

--- angular_new/settings.py
+++ angular_new/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'src'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Berlin'
+TIME_ZONE = 'Europe/Amsterdam'
 
 USE_I18N = True
 
